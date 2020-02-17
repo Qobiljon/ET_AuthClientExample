01018fa3b0fa2396979908c466c242adbad81b31
Qobiljon <qobiljon.toshnazarov@gmail.com>
2020-02-18 03:10:27 +0900

    major bugfix (+ 'continue with' button added)

--- app/src/main/java/inha/nslab/easytrack/GoogleAuthActivity.java
+++ app/src/main/java/inha/nslab/easytrack/GoogleAuthActivity.java
@@ -6,7 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -25,14 +26,18 @@ import io.grpc.StatusRuntimeException;
 
 public class GoogleAuthActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN_WITH_GOOGLE = 101;
-    private static String TAG = "ET_AUTH_EXAMPLE_APP";
+    // private static String TAG = "ET_AUTH_EXAMPLE_APP";
     private GoogleSignInClient signInClient;
     private SignInButton signInButton;
+    private SignInButton continueWithGoogleAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_auth);
+
+        signInButton = findViewById(R.id.googleSignInButton);
+        continueWithGoogleAccountButton = findViewById(R.id.continueWithGoogleAccountButton);
 
         // Google login client setup
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -40,19 +45,93 @@ public class GoogleAuthActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
         signInClient = GoogleSignIn.getClient(this, googleSignInOptions);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
-        signInButton = findViewById(R.id.googleSignInButton);
-        signInButton.setOnClickListener(view -> startGoogleAuthenticationActivity());
-
-        if (account == null)
-            startGoogleAuthenticationActivity();
-        else {
+        signInButton.setOnClickListener(view -> {
+            continueWithGoogleAccountButton.setVisibility(View.GONE);
             signInButton.setEnabled(false);
-            signInClient.signOut().addOnCompleteListener((Void) -> {
-                signInButton.setEnabled(true);
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            if (account == null)
                 startGoogleAuthenticationActivity();
-            });
+            else
+                signInClient.signOut().addOnSuccessListener(v -> {
+                    continueWithGoogleAccountButton.setVisibility(View.GONE);
+                    setGoogleSignInButtonText(signInButton, "Sign in");
+                    startGoogleAuthenticationActivity();
+                }).addOnFailureListener(e -> {
+                    GoogleSignInAccount _account = GoogleSignIn.getLastSignedInAccount(this);
+                    assert _account != null;
+                    continueWithGoogleAccountButton.setVisibility(View.VISIBLE);
+                    signInButton.setEnabled(true);
+                    setGoogleSignInButtonText(continueWithGoogleAccountButton, "Continue as: " + _account.getEmail());
+                    setGoogleSignInButtonText(signInButton, "Sign in with different account");
+                });
+        });
+        continueWithGoogleAccountButton.setOnClickListener(view -> {
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            assert account != null;
+            continueWithGoogleAccountButton.setEnabled(false);
+            signInButton.setEnabled(false);
+            new Thread(() -> {
+                try {
+                    ManagedChannel channel = ManagedChannelBuilder.forAddress(
+                            getString(R.string.grpc_host),
+                            Integer.parseInt(getString(R.string.grpc_port))
+                    ).usePlaintext().build();
+
+                    ETServiceGrpc.ETServiceBlockingStub stub = ETServiceGrpc.newBlockingStub(channel);
+                    EtService.LoginWithGoogleIdTokenRequestMessage requestMessage = EtService.LoginWithGoogleIdTokenRequestMessage.newBuilder()
+                            .setIdToken(account.getIdToken())
+                            .build();
+                    EtService.LoginResponseMessage responseMessage = stub.loginWithGoogleId(requestMessage);
+
+                    try {
+                        channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (responseMessage.getDoneSuccessfully())
+                        runOnUiThread(() -> {
+                            Intent result = new Intent("etAuthResult");
+                            result.putExtra("fields", "idToken,fullName,email,userId");
+                            result.putExtra("idToken", account.getIdToken());
+                            result.putExtra("fullName", account.getEmail());
+                            result.putExtra("email", account.getEmail());
+                            result.putExtra("userId", responseMessage.getUserId());
+                            setResult(Activity.RESULT_OK, result);
+                            finish();
+                        });
+                    else
+                        // technical issue, shouldn't happen
+                        runOnUiThread(() -> signInClient.signOut().addOnCompleteListener((Void) -> {
+                            Intent result = new Intent("etAuthResult");
+                            result.putExtra("fields", "note");
+                            result.putExtra("note", String.format(Locale.getDefault(), "please contact the EasyTrack developers with the following details: success=%b, userId=%d", responseMessage.getDoneSuccessfully(), responseMessage.getUserId()));
+                            setResult(Activity.RESULT_CANCELED, result);
+                            finish();
+                        }));
+                } catch (StatusRuntimeException e) {
+                    runOnUiThread(() -> {
+                        Intent result = new Intent("etAuthResult");
+                        result.putExtra("fields", "exception_message,exception_details");
+                        result.putExtra("exception_message", e.getMessage());
+                        result.putExtra("exception_details", e.toString());
+                        setResult(Activity.RESULT_CANCELED, result);
+                        finish();
+                    });
+                }
+            }).start();
+        });
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account == null) {
+            setGoogleSignInButtonText(signInButton, "Sign in");
+            continueWithGoogleAccountButton.setVisibility(View.GONE);
+            startGoogleAuthenticationActivity();
+        } else {
+            setGoogleSignInButtonText(signInButton, "Sign in with different account");
+            continueWithGoogleAccountButton.setVisibility(View.VISIBLE);
+            setGoogleSignInButtonText(continueWithGoogleAccountButton, "Continue with: " + account.getEmail());
         }
     }
 
@@ -60,7 +139,6 @@ public class GoogleAuthActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN_WITH_GOOGLE) {
-            signInButton.setEnabled(false);
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
@@ -128,6 +206,16 @@ public class GoogleAuthActivity extends AppCompatActivity {
                 result.putExtra("exception_details", e.toString());
                 setResult(Activity.RESULT_CANCELED, result);
                 finish();
+            }
+        }
+    }
+
+    private void setGoogleSignInButtonText(SignInButton button, String text) {
+        for (int n = 0; n < button.getChildCount(); n++) {
+            View view = button.getChildAt(n);
+            if (view instanceof TextView) {
+                ((TextView) view).setText(text);
+                return;
             }
         }
     }
